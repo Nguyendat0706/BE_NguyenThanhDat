626a53db78da9a18f8d3750dc1032e371f7e9010
Nguyendat0706 <duongthanhdat2k6@gmail.com>
2025-10-19 21:24:39 +0700

    BE2

---
+++ BE_NguyenThanhDat/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+using BE_NguyenThanhDat.Models;
+
+namespace BE_NguyenThanhDat.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("MyStoreDB") { }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BE_NguyenThanhDat/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BE_NguyenThanhDat.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string CategoryName { get; set; }
+    }
+}
